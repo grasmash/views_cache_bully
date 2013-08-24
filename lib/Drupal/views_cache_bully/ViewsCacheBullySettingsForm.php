f89e627e00ee6ec1fa2fb07ffaa2a02069a09e24
Matthew Grasmick <matthew.grasmick@acquia.com>
2013-08-24 16:39:11 -0400

    Fixing settings form. Defaults still dont load.

--- lib/Drupal/views_cache_bully/ViewsCacheBullySettingsForm.php
+++ lib/Drupal/views_cache_bully/ViewsCacheBullySettingsForm.php
@@ -34,20 +34,20 @@ class ViewsCacheBullySettingsForm extends SystemConfigFormBase {
       '#title' => t('Query results'),
       '#description' => t('The length of time raw query results should be cached.'),
       '#options' => $options,
-      '#default_value' => $config->get('views_cache_bully_results_lifespan'),
+      '#default_value' => $config->get('results_lifespan'),
     );
     $form['views_cache_bully_output_lifespan'] = array(
       '#type' => 'select',
       '#title' => t('Rendered output'),
       '#description' => t('The length of time rendered HTML output should be cached.'),
       '#options' => $options,
-      '#default_value' => $config->get('views_cache_bully_output_lifespan'),
+      '#default_value' => $config->get('output_lifespan'),
     );
     $form['views_cache_bully_exempt_exposed'] = array(
       '#type' => 'checkbox',
       '#title' => t('Exempt all views with exposed forms from bullying'),
       '#description' => t('Views using exposed forms will not be cached by Views Cache Bully.'),
-      '#default_value' => $config->get('views_cache_bully_exempt_exposed'),
+      '#default_value' => $config->get('exempt_exposed'),
     );
 
     $views = drupal_map_assoc(array_keys(views_get_all_views()));
@@ -56,7 +56,7 @@ class ViewsCacheBullySettingsForm extends SystemConfigFormBase {
       '#title' => t('Exempt the following views from bullying'),
       '#description' => t('Checked views will not be cached.'),
       '#options' => $views,
-      '#default_value' => $config->get('views_cache_bully_exemptions') ? $config->get('views_cache_bully_exemptions') : array(),
+      '#default_value' => $config->get('exemptions') ? $config->get('exemptions') : array(),
     );
 
     return parent::buildForm($form, $form_state);
@@ -66,8 +66,9 @@ class ViewsCacheBullySettingsForm extends SystemConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, array &$form_state) {
+    $config = $this->configFactory->get('views_cache_bully.settings');
 
-    Drupal::config('views_cache_bully.settings')
+    $config
       ->set('results_lifespan', $form_state['values']['views_cache_bully_results_lifespan'])
       ->set('output_lifespan', $form_state['values']['views_cache_bully_output_lifespan'])
       ->set('exempt_exposed', $form_state['values']['views_cache_bully_exempt_exposed'])
